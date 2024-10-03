66f7a160fe9a2bd1c80e7901a67dfb76dafd2929
jospin6 <jospinndagano1@gmail.com>
2024-10-03 09:52:32 +0200

    Use best practice approche to better organize our code

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    sessions: 'users/sessions',
-    registrations: 'users/registrations'
-  }
+  namespace :api do
+    namespace :v1 do
+      devise_for :users, controllers: {
+        registrations: 'api/v1/users/registrations',
+        sessions: 'api/v1/users/sessions'
+      }
+
+      post 'resend_confirmation_code', to: 'api/v1/users#resend_confirmation_code'
+    end
+  end
 end
