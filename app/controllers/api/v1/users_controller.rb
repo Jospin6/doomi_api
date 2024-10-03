d30bbc871135a3ebf26af0c059f56687fa33071a
jospin6 <jospinndagano1@gmail.com>
2024-10-03 11:22:57 +0200

    Add confirm_code function to the user controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,13 +1,23 @@
 class Api::V1::UsersController < ApplicationController
-    def resend_confirmation_code
-        @user = User.find_by(email: params[:email])
-        if @user
-          @user.generate_confirmation_code
-          @user.save
-          UserMailer.confirmation_email(@user).deliver_now
-          render json: { message: 'Un nouveau code de confirmation a été envoyé à votre e-mail.' }, status: :ok
-        else
-          render json: { error: 'Aucun utilisateur trouvé avec cet e-mail.' }, status: :not_found
-        end
+  def confirm_code
+    @user = User.find_by(email: params[:email])
+    if @user && @user.confirmation_code == params[:confirmation_code]
+      @user.update(confirmed_at: Time.current) 
+      render json: { message: 'Votre compte a été confirmé!' }, status: :ok
+    else
+      render json: { error: 'Code de confirmation invalide.' }, status: :unprocessable_entity
     end
+  end
+
+  def resend_confirmation_code
+      @user = User.find_by(email: params[:email])
+      if @user
+        @user.generate_confirmation_code
+        @user.save
+        UserMailer.confirmation_email(@user).deliver_now
+        render json: { message: 'Un nouveau code de confirmation a été envoyé à votre e-mail.' }, status: :ok
+      else
+        render json: { error: 'Aucun utilisateur trouvé avec cet e-mail.' }, status: :not_found
+      end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    registrations: 'api/v1/users/registrations',
-    sessions: 'api/v1/users/sessions'
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
   }
   namespace :api do
     namespace :v1 do
-      post 'resend_confirmation_code', to: 'api/v1/users#resend_confirmation_code'
+      post 'confirm_code', to: 'users#confirm_code'
+      post 'resend_confirmation_code', to: 'users#resend_confirmation_code'
     end
   end
 end
