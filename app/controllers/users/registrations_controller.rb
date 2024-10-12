ee21d7e5d73fe52117996a703b041331d12edb72
jospin6 <jospinndagano1@gmail.com>
2024-10-12 17:40:10 +0200

    Config mailing in the development mode

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,31 +3,21 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   respond_to :json
 
-  def create
-    build_resource
-
-    if resource.save
-      UserMailer.confirmation_email(resource).deliver_now
-      render json: { message: 'Utilisateur créé. Vérifiez votre e-mail pour le code de confirmation.' }, status: :created
-    else
-      render json: { errors: resource.errors.full_messages }, status: :unprocessable_entity
-    end
-  end
-
   private
 
-  # def respond_with(resource, options={})
-  #   if resource.persisted?
-  #     render json: {
-  #       code: 200,
-  #       message: 'signed up successfully',
-  #       data: resource
-  #     }, status: :ok
-  #   else
-  #     render json: {
-  #       message: 'User could not be created successfully',
-  #       errors: resource.errors.full_messages
-  #     }, status: :unprocessable_entity     
-  #   end
-  # end 
+  def respond_with(resource, options={})
+    if resource.persisted?
+      UserMailer.confirmation_email(resource).deliver_now
+      render json: {
+        code: 200,
+        message: 'signed up successfully',
+        data: resource
+      }, status: :ok
+    else
+      render json: {
+        message: 'User could not be created successfully',
+        errors: resource.errors.full_messages
+      }, status: :unprocessable_entity     
+    end
+  end 
 end
