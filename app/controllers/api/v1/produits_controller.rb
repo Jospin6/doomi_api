30da24148dc49c8ae36f147580561756450de6db
jospin6 <jospinndagano1@gmail.com>
2024-11-05 13:18:21 +0200

    Correct the create produit function

--- app/controllers/api/v1/produits_controller.rb
+++ app/controllers/api/v1/produits_controller.rb
@@ -3,7 +3,12 @@ class Api::V1::ProduitsController < ApplicationController
 
   # GET /produits
   def index
-    @produits = Produit.all
+    @produits = Produit.includes(:vetementChaussure, 
+    :vehicule, 
+    :immobilier, 
+    :evenement, 
+    :emploi, 
+    :autreProduitAttribut).all
 
     render json: @produits
   end
@@ -30,6 +35,18 @@ class Api::V1::ProduitsController < ApplicationController
     @produit = Produit.new(produit_params)
 
     if @produit.save
+      if params[:vehicule] || params[:autreProduitAttribut]
+        @produit.build_vehicule(vehicule_params)
+        @produit.build_autreProduitAttribut(autreProduitAttribut_params)
+      elsif params[:immobilier]
+        @produit.build_immobilier(immobilier_params)
+      elsif params[:evenement]
+        @produit.build_evenement(evenement_params)
+      elsif params[:emploi]
+        @produit.build_emploi(emploi_params)    
+      else
+        @produit.build_autreProduitAttribut(autreProduitAttribut_params)
+      end
       render json: @produit, status: :created
     else
       render json: @produit.errors, status: :unprocessable_entity
@@ -60,4 +77,25 @@ class Api::V1::ProduitsController < ApplicationController
     def produit_params
       params.require(:produit).permit(:titre, :prix, :description, :sub_categorie_produit_id, :user_id, images: [])
     end
+
+    def vehicule_params
+      params.require(:vehicule).permit(:produit, :modele, :annee, :kilometrage, :type_vehicule, :couleur, :carburant, :transmission, :nombre_portes, :nombre_places, :statut, :plan_de_paiement, :disponibilite)
+    end
+
+    def immobilier_params
+      params.require(:immobilier).permit(:produit, :type_de_bien, :adresse, :surface_habitable, :nombre_chambres, :nombre_pieces)
+    end
+
+    def evenement_params
+      params.require(:evenement).permit(:produit, :date_evenement, :lieu, :type_prix, :etat_evenement)
+    end
+
+    def emploi_params
+      params.require(:emploi).permit(:produit, :type_contrat, :lieu, :secteur_activite, :niveau_experience, :date_limite, :salaire, :site_web, :formation_requise, :etat_offre)
+    end
+
+    def autreProduitAttribut_params
+      params.require(:autreProduitAttribut).permit(:produit, :etat, :marque)
+    end
+    
 end
