073ddb1d823fd9173a5b78c4be561edf39a94259
jospin6 <jospinndagano1@gmail.com>
2024-11-11 09:56:25 +0200

    Add devise to the hotellerie service

--- db/migrate/20241107081534_create_hotellerie_services.rb
+++ db/migrate/20241107081534_create_hotellerie_services.rb
@@ -2,7 +2,7 @@ class CreateHotellerieServices < ActiveRecord::Migration[7.0]
   def change
     create_table :hotellerie_services do |t|
       t.string :nom_hotel
-      t.string :type_hebergement
+      t.text :type_hebergement
       t.text :adresse
       t.text :contacts
       t.text :email
@@ -10,6 +10,7 @@ class CreateHotellerieServices < ActiveRecord::Migration[7.0]
       t.integer :nombre_chambres
       t.integer :capacite_accueil
       t.decimal :tarif_moyen
+      t.integer :devise
       t.text :horaires
       t.string :localisation
       t.references :user, null: false, foreign_key: true
