fca77c48621fb2b039a4fa31f3e217894f5f3e39
jospin6 <jospinndagano1@gmail.com>
2024-11-11 09:57:51 +0200

    Change some attributs to null into hotellerie_services table

--- db/migrate/20241107081534_create_hotellerie_services.rb
+++ db/migrate/20241107081534_create_hotellerie_services.rb
@@ -6,11 +6,11 @@ class CreateHotellerieServices < ActiveRecord::Migration[7.0]
       t.text :adresse
       t.text :contacts
       t.text :email
-      t.text :site_web
+      t.text :site_web, null: false
       t.integer :nombre_chambres
       t.integer :capacite_accueil
-      t.decimal :tarif_moyen
-      t.integer :devise
+      t.decimal :tarif_moyen, null: false
+      t.integer :devise, null: false
       t.text :horaires
       t.string :localisation
       t.references :user, null: false, foreign_key: true
