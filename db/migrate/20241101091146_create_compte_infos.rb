edfcf6937fabb8870d903410257f75e5db72a5a2
jospin6 <jospinndagano1@gmail.com>
2024-11-11 10:09:37 +0200

    Add role to the table compte_info

--- db/migrate/20241101091146_create_compte_infos.rb
+++ db/migrate/20241101091146_create_compte_infos.rb
@@ -6,6 +6,7 @@ class CreateCompteInfos < ActiveRecord::Migration[7.0]
       t.datetime :confirmed_at, null: true
       t.string :rccm, null: true, default: ""
       t.string :langue, null: true, default: ""
+      t.string :role, default: "utilisateur"
 
       t.timestamps
     end
