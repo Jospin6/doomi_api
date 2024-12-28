4f341e61e19dba98308a88e9a4230336fdbf3a79
jospin6 <jospinndagano1@gmail.com>
2024-12-28 15:11:59 +0200

    get user products

--- app/controllers/api/v1/produits_controller.rb
+++ app/controllers/api/v1/produits_controller.rb
@@ -104,16 +104,88 @@ class Api::V1::ProduitsController < ApplicationController
 
     @produits = CategorieProduit.includes(
     sub_categorie_produits: {
-        produits: [:vetement_chaussure, 
-                   :vehicule, 
-                   :immobilier, 
-                   :evenement, 
-                   :emploi, 
-                   :autre_produit_attribut]
+        produits: {
+          vetement_chaussure: {}, 
+          vehicule: {}, 
+          immobilier: {}, 
+          evenement: {}, 
+          emploi: {}, 
+          autre_produit_attribut: {},
+          service: {},
+          images: [],
+          user: {}
+        }
     }
-    ).where(sub_categorie_produits: { produits: { user_id: current_user.id } })
+    ).where(sub_categorie_produits: { produits: { user_id: params[:id] } })
 
-    render json: @produits
+    render json: @produits.as_json(include: {
+      images: {
+              only: [
+                :id, 
+                :imageable_type, 
+                :imageable_id]
+            },
+            vetement_chaussure: {
+              only: [
+                :type_vetement, 
+                :taille, 
+                :matiere]
+            },
+            vehicule: {
+              only: [
+                :modele, 
+                :annee, 
+                :kilometrage, 
+                :type_vehicule, 
+                :couleur, 
+                :carburant, 
+                :transmission, 
+                :nombre_portes, 
+                :nombre_places, 
+                :statut, 
+                :plan_de_paiement, 
+                :disponibilite]
+            },
+            immobilier: {
+              only: [
+                :type_de_bien, 
+                :adresse, 
+                :surface_habitable, 
+                :nombre_chambres, 
+                :nombre_pieces]
+            },
+            evenement: {
+              only: [
+                :date_evenement, 
+                :lieu, 
+                :type_prix, 
+                :site_web, 
+                :etat_evenement]
+            },
+            emploi: {
+              only: [
+                :type_contrat, 
+                :lieu, 
+                :secteur_activite, 
+                :niveau_experience, 
+                :date_limite, 
+                :site_web, 
+                :formation_requise, 
+                :etat_offre]
+            },
+            autre_produit_attribut: {
+              only: [
+                :etat, 
+                :marque]
+            },
+            service: {
+              only: [:statut]
+            },
+            user: {
+              only: [:id, :username]
+            },
+            method: [:image_urls]
+    })
   end
   
 
